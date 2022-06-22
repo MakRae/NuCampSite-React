b77f296ed12c7a0dcca2b74e7638a6e3a1a56f35
Makayla Paasch <kaydarae19@gmail.com>
2022-06-22 15:51:35 -0500

    added react style

--- src/features/display/DisplayList.js
+++ src/features/display/DisplayList.js
@@ -1,30 +1,8 @@
 import { Col, Row } from "reactstrap";
-import DisplayCard from "./DisplayCard";
+import AnimatedDisplayCard from "./AnimatedDisplayCard";
 import { selectFeaturedCampsite } from "../campsites/campsitesSlice";
 import { selectFeaturedPromotion } from "../promotions/promotionsSlice";
 import { selectFeaturedPartner } from "../partners/partnersSlice";
-
-// const DisplayList = () => {
-//   const items = (state) => [
-//     selectFeaturedCampsite(state),
-//     selectFeaturedPromotion(state),
-//     selectFeaturedPartner(state),
-//   ];
-
-//   console.log("display items:", items);
-
-//   return (
-//     <Row>
-//       {items.map((item, idx) => {
-//         return (
-//           <Col md className="m-1" key={idx}>
-//             <DisplayCard item={item} />
-//           </Col>
-//         );
-//       })}
-//     </Row>
-//   );
-// };
 
 const DisplayList = () => {
   const items = [
@@ -37,9 +15,11 @@ const DisplayList = () => {
     <Row>
       {items.map((item, idx) => {
         return (
-          <Col md className="m-1" key={idx}>
-            <DisplayCard item={item} />
-          </Col>
+          item && (
+            <Col md className="m-1" key={idx}>
+              <AnimatedDisplayCard item={item} />
+            </Col>
+          )
         );
       })}
     </Row>
